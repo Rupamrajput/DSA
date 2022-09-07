a5c0d87631c09cc596487805739eac2ae34076b6
Rupam <rupamraj91100@gmail.com>
2022-09-07 17:00:38 +0530

    ninthDay

--- src/ArraysQues/BuyAndSellStock.java
+++ src/ArraysQues/BuyAndSellStock.java
@@ -5,7 +5,7 @@ public class BuyAndSellStock {
         int maxSoFar = 0;
         int currMaxDiff = 0;
         for(int i=1; i<prices.length;i++){
-            currMaxDiff = Math.max(0,currMaxDiff+=prices[i]-prices[i-1]);
+            currMaxDiff = Math.max(0, currMaxDiff + (prices[i] - prices[i - 1]));
             maxSoFar = Math.max(currMaxDiff,maxSoFar);
         }
         return maxSoFar;
---
+++ src/ArraysQues/MaxSubArrayProduct.java
@@ -0,0 +1,16 @@
+package ArraysQues;
+
+public class MaxSubArrayProduct {
+    public static int maxProduct(int[] nums) {
+        int currMax = nums[0],currMin=nums[0];
+        int result = nums[0];
+        for(int j =1;j<nums.length;j++){
+            int i = nums[j];
+            int temp = currMax;
+            currMax = Math.max(Math.max(currMax*i,currMin*i),i);
+            currMin = Math.min(Math.min(temp*i,currMin*i),i);
+            result = Math.max(result,currMax);
+        }
+        return result;
+    }
+}
